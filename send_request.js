cc05485edad137addd857d7cd83a66c2fa953ff5
Kami <harshimymommy@gmail.com>
2024-01-04 19:26:38 +0000

    feat: ✨ Swapped domain names

--- send_request.js
+++ send_request.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch");
+const WebSocket = require("ws");
 const formData = 19;
 (async () => {
+const ws = new WebSocket("ws://localhost:777");
 const response = await fetch("http://api.kami-x.tk/bot_status", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
